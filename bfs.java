367896a05e1e56ab789830b2c553b0380df58542
Jeremy Su <70295411+bulletchicken@users.noreply.github.com>
2023-03-21 10:57:58 -0400

    Update bfs.java

--- bfs.java
+++ bfs.java
@@ -1,8 +1,11 @@
 
 public class ShortestPathFinder {
+	static int shorty = -1;
+	
 	public static void main(String[]args){
+		
 		boolean[][] maze = 
-			{ {true, false, false, false, false},
+			{		{true, false, false, false, false},
 					{true, true, true, false, false},
 					{true, false, true, false, false},
 					{true, true, true, true, true},
@@ -14,57 +17,59 @@ public class ShortestPathFinder {
 		int startingY = 1; //row 1
 
 		int endingX = 0; //column 0
-		int endingY = 4; //row 4
+		int endingY =5; //row 4
+		
 
-		int shortestPath = bfs(startingY, startingX, endingY, endingX, maze, 0);
-		System.out.println(shortestPath);
+
+		bfs(startingY, startingX, endingY, endingX, maze, 1);
+		System.out.println(shorty);
+		
 	}
 
 	public static int bfs(int currentY, int currentX, int endingY, int endingX, boolean[][]maze, int shortest){
-		shortest = 0;
-
+		
 		boolean checked [][] = maze; //creating a pbv as to not mess up. Used to keep track of the current stream being searched
 
 		int columnSize = maze.length;
 		int rowSize = maze[0].length;
-
+		System.out.println("Coordinates at: {" + currentX+ ", " + currentY + "}");
+		System.out.println(shortest);
+		
+		checked[currentY][currentX] = false;
+		
 		if((currentY == endingY)&&(currentX == endingX)){
-			return shortest;
+			shorty = shortest;
+			return -1;
 		}
-		System.out.println(currentX);
-		System.out.println(currentY);
+		
 
 		if(currentX>0){//check for left border
-			if((maze[currentY][currentX-1] == true) && (checked[currentY][currentX-1] == false)){
-				shortest++;
-				checked[currentY][currentX-1] = true;
-				bfs(currentY, currentX-1, endingY, endingX, maze, shortest);
+			if((maze[currentY][currentX-1] == true) && (checked[currentY][currentX-1] == true)){
+				bfs(currentY, currentX-1, endingY, endingX, maze, shortest+1);
 			}
 		}
 
-		if(currentX<rowSize){ //check for right border
-			if((maze[currentY][currentX+1] == true)&& (checked[currentY][currentX+1] == false)){
-
-				shortest++;
-				bfs(currentY, currentX+1, endingY, endingX, maze, shortest);
+		if(currentX<rowSize-1){ //check for right border
+			if((maze[currentY][currentX+1] == true)&& (checked[currentY][currentX+1] == true)){
+				bfs(currentY, currentX+1, endingY, endingX, maze, shortest+1);
 			}
 		}
 
-		if(currentY>0){
-			if((maze[currentY-1][currentX] == true) &&(checked[currentY][currentX+1] == false)){
+		if(currentY>0){ //check up
 
+			if((maze[currentY-1][currentX] == true) &&(checked[currentY-1][currentX] == true)){
+				bfs(currentY-1, currentX, endingY, endingX, maze, shortest+1);
 			}
-			shortest++;
-			bfs(currentY, currentX-1, endingY, endingX, maze, shortest);
+
 		}
-		if(currentY<columnSize){
-			if((maze[currentY+1][currentX] == true)&& (checked[currentY][currentX+1] == false)){
-				shortest++;
-				bfs(currentY+1, currentX, endingY, endingX, maze, shortest);
+		
+		
+		if(currentY<columnSize-1){ //check down
+			if((maze[currentY+1][currentX] == true)&& (checked[currentY+1][currentX] == true)){
+
+				bfs(currentY+1, currentX, endingY, endingX, maze, shortest+1);
 			}
 		}
-
-
 
 		return -1;
 
