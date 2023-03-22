a43630ffe4c2186730a0c04b36076311bc039eea
Jeremy Su <70295411+bulletchicken@users.noreply.github.com>
2023-03-21 23:03:25 -0400

    Update bfs.java

--- bfs.java
+++ bfs.java
@@ -1,7 +1,5 @@
 
 public class ShortestPathFinder {
-	static int shorty = -1;
-	
 	public static void main(String[]args){
 		
 		boolean[][] maze = 
@@ -16,18 +14,18 @@ public class ShortestPathFinder {
 		int startingX = 2; //colulmn 2
 		int startingY = 1; //row 1
 
-		int endingX = 0; //column 0
-		int endingY =5; //row 4
+		int endingX = 2; //column 0
+		int endingY =2; //row 4
 		
 
 
-		bfs(startingY, startingX, endingY, endingX, maze, 1);
-		System.out.println(shorty);
+		System.out.println(bfs(startingY, startingX, endingY, endingX, maze, 0, false));
 		
 	}
 
-	public static int bfs(int currentY, int currentX, int endingY, int endingX, boolean[][]maze, int shortest){
+	public static int bfs(int currentY, int currentX, int endingY, int endingX, boolean[][]maze, int shortest, boolean found){
 		
+        
 		boolean checked [][] = maze; //creating a pbv as to not mess up. Used to keep track of the current stream being searched
 
 		int columnSize = maze.length;
@@ -36,42 +34,69 @@ public class ShortestPathFinder {
 		System.out.println(shortest);
 		
 		checked[currentY][currentX] = false;
-		
-		if((currentY == endingY)&&(currentX == endingX)){
-			shorty = shortest;
-			return -1;
-		}
-		
-
-		if(currentX>0){//check for left border
-			if((maze[currentY][currentX-1] == true) && (checked[currentY][currentX-1] == true)){
-				bfs(currentY, currentX-1, endingY, endingX, maze, shortest+1);
-			}
-		}
-
-		if(currentX<rowSize-1){ //check for right border
-			if((maze[currentY][currentX+1] == true)&& (checked[currentY][currentX+1] == true)){
-				bfs(currentY, currentX+1, endingY, endingX, maze, shortest+1);
-			}
-		}
-
-		if(currentY>0){ //check up
-
-			if((maze[currentY-1][currentX] == true) &&(checked[currentY-1][currentX] == true)){
-				bfs(currentY-1, currentX, endingY, endingX, maze, shortest+1);
-			}
-
-		}
-		
-		
-		if(currentY<columnSize-1){ //check down
-			if((maze[currentY+1][currentX] == true)&& (checked[currentY+1][currentX] == true)){
-
-				bfs(currentY+1, currentX, endingY, endingX, maze, shortest+1);
-			}
-		}
-
-		return -1;
-
-	}
+		if(found){
+            return shortest; //zero base
+        }
+        if((currentY == endingY)&&(currentX == endingX)){
+            System.out.println("found");
+            found = true;
+            return shortest; //found the shortest path and should exit the bfs
+        }
+        
+    
+        int shorty = -1;
+        if(currentX>0){//check for left border
+            if((maze[currentY][currentX-1] == true)){
+                int res = bfs(currentY, currentX-1, endingY, endingX, checked, shortest+1, found);
+                if (res != -1 && (shorty == -1 || res < shorty)) {
+                    shorty = res;
+                }
+                if(found){
+                    return shortest; //zero base
+                }
+            }
+        }
+    
+        if(currentX<rowSize-1){ //check for right border
+            if((maze[currentY][currentX+1] == true)){
+                int res = bfs(currentY, currentX+1, endingY, endingX, checked, shortest+1, found);
+                if (res != -1 && (shorty == -1 || res < shorty)) {
+                    shorty = res;
+                }
+                if(found){
+                    return shortest; //zero base
+                }
+            }
+        }
+    
+        if(currentY>0){ //check up
+    
+            if((maze[currentY-1][currentX] == true)){
+                int res = bfs(currentY-1, currentX, endingY, endingX, checked, shortest+1, found);
+                if (res != -1 && (shorty == -1 || res < shorty)) {
+                    shorty = res;
+                }
+                if(found){
+                    return shortest; //zero base
+                }
+            }
+    
+        }
+    
+    
+        if(currentY<columnSize-1){ //check down
+            if((maze[currentY+1][currentX] == true)){
+                int res = bfs(currentY+1, currentX, endingY, endingX, checked, shortest+1, found);
+                if (res != -1 && (shorty == -1 || res < shorty)) {
+                    shorty = res;
+                }
+                if(found){
+                    return shortest; //zero base
+                }
+            }
+        }
+    
+        return shorty;
+        //shorty is the shortest for each branch essentially after reaching the end
+    }
 }
